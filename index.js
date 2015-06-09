0303ebe88c4263a15d924eb4a4a43b768153f34c
Conor McNamara <conortm@gmail.com>
2015-06-08 22:07:19 -0400

    Add stream support

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 'use strict';
 
-var jsonServer = require('json-server');
-var gutil = require('gulp-util');
 var extend = require('extend-shallow');
+var gutil = require('gulp-util');
+var jsonServer = require('json-server');
+var through = require('through2');
 
 module.exports = function(options) {
-	if (!options) {
-		throw new gutil.PluginError('gulp-json-server', 'Missing options.');
-	}
-	if (!options.source) {
-		throw new gutil.PluginError('gulp-json-server', 'Missing source option.');
-	}
+
 	options = extend({
 		hostname: 'localhost',
 		port: 3000
-	}, options);
-	var server = jsonServer.create();
-	server.use(jsonServer.defaults)
-		.use(jsonServer.router(options.source))
-		.listen(options.port, options.hostname)
-		.on('listening', function() {
-			gutil.log('JSON server started at', gutil.colors.cyan('http://' + options.hostname + ':' + options.port));
-		})
-		.on('error', function(err) {
-			if (err.code === 'EADDRINUSE') {
-				throw new gutil.PluginError('gulp-json-server', 'Port ' + options.port + ' is already in use by another process.');
-			} else {
-				throw new gutil.PluginError('gulp-json-server', err);
-			}
-		});
+	}, (options || {}));
+
+	var stream = through.obj(function(file, enc, callback) {
+
+		var server = jsonServer.create();
+
+		server.use(jsonServer.defaults)
+			.use(jsonServer.router(file.path))
+			.listen(options.port, options.hostname)
+			.on('listening', function() {
+				gutil.log('JSON server started at', gutil.colors.cyan('http://' + options.hostname + ':' + options.port));
+			})
+			.on('error', function(err) {
+				if (err.code === 'EADDRINUSE') {
+					throw new gutil.PluginError('gulp-json-server', 'Port ' + options.port + ' is already in use by another process.');
+				} else {
+					throw new gutil.PluginError('gulp-json-server', err);
+				}
+			});
+
+		this.push(file);
+		callback();
+	});
+
+	return stream;
 };
--- test.js
+++ test.js
@@ -2,12 +2,25 @@
 
 var request = require('supertest');
 var jsonServer = require('./');
+var File = require('gulp-util').File;
 
 describe('gulp-json-server', function() {
 
-	it('should work', function(done) {
+	var stream;
 
-		jsonServer({source: 'fixtures/db.json'});
+	var dbFile = new File({
+		path: __dirname + '/fixtures/db.json'
+	});
+
+	afterEach(function() {
+		stream.emit('kill');
+	});
+
+	it('should work with default options', function(done) {
+
+		stream = jsonServer();
+
+		stream.write(dbFile);
 
 		request('http://localhost:3000')
 			.get('/posts/1')
