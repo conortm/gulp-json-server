e124955dba03e100bfe36300b4ac661402fccd26
Conor McNamara <conortm@gmail.com>
2015-06-18 13:09:21 -0400

    Implement watch, fix #3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var extend = require('extend-shallow');
+var fs = require('fs');
 var gutil = require('gulp-util');
 var jsonServer = require('json-server');
 var through = require('through2');
@@ -9,15 +10,32 @@ module.exports = function(options) {
 
 	options = extend({
 		hostname: 'localhost',
-		port: 3000
+		port: 3000,
+		watch: false
 	}, (options || {}));
 
 	var stream = through.obj(function(file, enc, callback) {
 
 		var server = jsonServer.create();
+		var router = jsonServer.router(file.path);
+
+		if (options.watch) {
+			fs.watch(file.path, function (event, changedFile) {
+				if (event === 'change') {
+					gutil.log(gutil.colors.magenta(file.path), 'has changed, reloading json-server database');
+					try {
+						router.db.object = JSON.parse(fs.readFileSync(file.path));
+					} catch (err) {
+						gutil.log('Unable to parse', gutil.colors.magenta(file.path));
+						gutil.log(err);
+					}
+				}
+			});
+			gutil.log('Watching', gutil.colors.magenta(file.path));
+	  }
 
 		server.use(jsonServer.defaults)
-			.use(jsonServer.router(file.path))
+			.use(router)
 			.listen(options.port, options.hostname)
 			.on('listening', function() {
 				gutil.log('JSON server started at', gutil.colors.cyan('http://' + options.hostname + ':' + options.port));
